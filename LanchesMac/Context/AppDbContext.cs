d3ac33457c00a2f8186d25cf669156746b5242a1
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-08-03 21:30:28 -0300

    Configurando a migração

---
+++ LanchesMac/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //DbContextOptions define as opções a serem usadas
+            //pelo dbContext. Ela vai carregar as informações de configuração necessária para
+            //carregar o dbContext
+
+        }
+
+          //dbSet define quais classes quer mapear para quais tabelas
+
+        //As variáveis abaixo iram virar tabelas
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
