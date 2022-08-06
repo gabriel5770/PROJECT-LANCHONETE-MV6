59276448f40a826f4e7dd74a7828017f2c4973aa
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-08-05 21:43:38 -0300

    Implementação das interfaces;

---
+++ LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+
+        //retorna uma coleção de objetos Categoria
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
